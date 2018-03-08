6a2e2b1bfd9c50252a6a77ef1867dd87043aed79
Dale Newman <dalenewman@gmail.com>
2018-03-07 22:29:45 -0500

    updated nuget batch file

--- nuget/nuget.push.bat
+++
@@ -1,4 +0,0 @@
-nuget push "c:\temp\modules\Transformalize.Provider.Bogus.0.3.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Bogus.Autofac.0.3.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
-
-
