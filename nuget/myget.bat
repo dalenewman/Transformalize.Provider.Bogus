788fadb88d6321b84ce0aabf3b12d10254cc7fad
Dale Newman <dalenewman@gmail.com>
2020-05-08 12:18:08 -0400

    updated myget with debug packages

---
+++ nuget/myget.bat
@@ -0,0 +1,7 @@
+nuget pack Transformalize.Provider.Bogus.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Provider.Bogus.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+
+nuget push "c:\temp\modules\Transformalize.Provider.Bogus.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Bogus.Autofac.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+
+
