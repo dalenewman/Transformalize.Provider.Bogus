8231ba4ff366d82c2a04f45611e81f4d99ace9c2
Dale Newman <dalenewman@gmail.com>
2020-10-22 21:07:53 -0400

    Update Transformalize Dependencies

--- src/Tests.Unit/Properties/AssemblyInfo.cs
+++ src/Tests.Unit/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("57a8d3b7-ec07-44a3-be58-1bd39b3c499f")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.22.0")]
-[assembly: AssemblyFileVersion("0.8.22.0")]
+[assembly: AssemblyVersion("0.8.29.0")]
+[assembly: AssemblyFileVersion("0.8.29.0")]
